147f1b4010f35b9fcdd402d08d878c94104d9797
komei otake <komei1220@gmail.com>
2017-10-24 20:26:46 +0900

    update main.ts

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,28 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import './sass/main.scss';
+import './styles/main.scss';
 
-import { HomeComponent } from './components/home';
-import { AboutComponent } from './components/about';
-import { ListComponent } from './components/list';
-import { NavbarComponent } from './components/navbar';
+import { App } from './components/containers/App/';
 
 // register the plugin
 Vue.use(VueRouter);
 
 let router = new VueRouter({
   routes: [
-    { path: '/', component: HomeComponent },
-    { path: '/about', component: AboutComponent },
-    { path: '/list', component: ListComponent },
-  ]
+    { path: '/', component: App }
 });
 
-new Vue({
-  el: '#app-main',
-  router: router,
-  components: {
-    'navbar': NavbarComponent
-  }
-});
