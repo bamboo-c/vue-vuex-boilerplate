73fc9337d3327f82b5eeb0d6753b0ae6c4499928
komei otake <komei1220@gmail.com>
2017-10-29 21:24:53 +0900

    changed template counter component

--- template/src/components/containers/Counter/CounterContainer.ts
+++ template/src/components/containers/Counter/CounterContainer.ts
@@ -1,21 +1,21 @@
 import Component from 'vue-class-component';
 import Vue  = require('vue');
-import {MutationTypes} from '../../vuex/mutation-types';
+import {MutationTypes} from '../../vuex/mutation-types/index';
 /**
  * Counter Component Vuex used
  */
 @Component({
-    name: 'counter',
-    template: require('./counter.pug'),
+  name: 'counter',
+  template: require('./counter.html'),
 })
 export class Counter extends Vue {
-    private back() {
-        this.$router.back();
-    }
-    private countUp() {
-        this.$store.dispatch(MutationTypes.COUNT_UP);
-    }
-    get count() {
-        return this.$store.state.count;
-    }
+  private back() {
+    this.$router.back();
+  }
+  private countUp() {
+    this.$store.dispatch(MutationTypes.COUNT_UP);
+  }
+  get count() {
+    return this.$store.state.count;
+  }
 }
