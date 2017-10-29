d673a54f01432c450e2b59683df7270b40e6a561
komei otake <komei1220@gmail.com>
2017-10-29 21:13:10 +0900

    add store

---
+++ template/src/store/index.ts
@@ -0,0 +1,25 @@
+import Vue = require('vue');
+import Vuex = require('vuex');
+import actions from './actions/index';
+import getters from './getters/index';
+import mutations from './mutations/index';
+import states from './states/index';
+
+Vue.use(Vuex);
+
+// A Vuex instance is created by combining the state, the actions,
+// and the mutations. Because the actions and mutations are just
+// functions that do not depend on the instance itself, they can
+// be easily tested or even hot-reloaded (see counter-hot example).
+
+const prod = (process.env.NODE_ENV === 'production');
+
+const store = new Vuex.Store({
+    states,
+    actions,
+    mutations,
+    getters,
+    strict: prod,
+});
+
+export default store;
