c4027b97aa92b00d5ed678c8e72a08cbad62057d
komei otake <komei1220@gmail.com>
2017-10-29 23:33:03 +0900

    update spec file

--- template/src/components/containers/Counter/Counter.spec.ts
+++ template/src/components/containers/Counter/Counter.spec.ts
@@ -4,81 +4,10 @@ import Component from 'vue-class-component';
 import { spy, assert } from 'sinon';
 import { expect } from 'chai';
 import { ComponentTest, MockLogger } from '../../../utils/component-test';
-import { Navbar } from './';
+import { CounterContainer } from './';
 
 let loggerSpy = spy();
 
 @Component({
-  template: require('./navbar.html')
+  template: require('./counter.html')
 })
-class MockNavbar extends Navbar {
-  constructor() {
-    super();
-    this.logger = new MockLogger(loggerSpy);
-  }
-}
-
-describe('Navbar component', () => {
-  let directiveTest: ComponentTest;
-  let router: VueRouter;
-
-  before(() => {
-    Vue.use(VueRouter);
-    directiveTest = new ComponentTest('<div><navbar></navbar><router-view>loading...</router-view></div>', { 'navbar': MockNavbar });
-
-    let AppContainer = { template: '<div class="app">Home</div>' };
-    let ListContainer = { template: '<div class="list">List</div>' };
-
-    router = new VueRouter({
-      routes: [
-        { path: '/', component: AppContainer },
-        { path: '/list', component: ListContainer }
-      ]
-    });
-  });
-
-  it('should render correct contents', async () => {
-    directiveTest.createComponent({ router: router });
-
-    await directiveTest.execute((vm) => { // ensure Vue has bootstrapped/run change detection
-      debugger;
-      assert.calledWith(loggerSpy, 'Default object property!');
-      expect(vm.$el.querySelectorAll('ul.nav li').length).to.equal(3);
-    });
-  });
-
-  describe('When clicking the about link', () => {
-    beforeEach(async () => {
-      directiveTest.createComponent({ router: router });
-
-      await directiveTest.execute((vm) => {
-        let anchor = <HTMLAnchorElement>vm.$el.querySelector('ul.nav li a[href="#/about"]');
-        anchor.click();
-      });
-    });
-
-    it('should render correct about contents', async () => {
-      await directiveTest.execute((vm) => {
-        expect(vm.$el.querySelector('div.about').textContent).to.equal('About');
-      });
-    });
-  });
-
-  describe('When clicking the list link', () => {
-    beforeEach(async () => {
-      directiveTest.createComponent({ router: router });
-
-      await directiveTest.execute((vm) => {
-        let anchor = <HTMLAnchorElement>vm.$el.querySelector('ul.nav li a[href="#/list"]');
-        anchor.click();
-      });
-    });
-
-    it('should render correct about contents', async () => {
-      await directiveTest.execute((vm) => {
-        expect(vm.$el.querySelector('div.list').textContent).to.equal('List');
-      });
-    });
-  });
-
-});
