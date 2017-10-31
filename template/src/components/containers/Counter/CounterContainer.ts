a8ab6d94f8fa0659308814836c84a419811d6bb6
komei otake <komei1220@gmail.com>
2017-10-31 09:10:51 +0900

    tweak

--- template/src/components/containers/Counter/CounterContainer.ts
+++ template/src/components/containers/Counter/CounterContainer.ts
@@ -1,23 +1,24 @@
-import Component from 'vue-class-component';
 import Vue from 'vue';
+import Component from 'vue-class-component';
 import {MutationTypes} from '../../../vuex/mutation-types/index';
-/**
- * Counter Component Vuex used
- */
+
 @Component({
-  name: 'counter',
   template: require('./counter.html'),
 })
 export class CounterContainer extends Vue {
+
   private incrementValue() {
     this.$store.dispatch(MutationTypes.INCREMENT_VALUE);
   }
+
   private decrementValue() {
     this.$store.dispatch(MutationTypes.DECREMENT_VALUE);
   }
+
   private resetValue() {
     this.$store.dispatch(MutationTypes.RESET_VALUE);
   }
+
   get count() {
     return this.$store.state.count;
   }
