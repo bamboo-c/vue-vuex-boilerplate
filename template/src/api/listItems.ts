5e2d9b8358ca840f7a980886dd64c775b0c08f91
komei otake <komei1220@gmail.com>
2017-11-02 08:28:36 +0900

    change file stracture

--- template/src/api/listItems.ts
+++ template/src/api/listItems.ts
@@ -1,3 +1,28 @@
+import axios, {AxiosResponse} from 'axios';
+
+interface UserResponse {
+  id: string;
+  name: string;
+}
+
+items: UserResponse[] = [];
+  private url = 'https://jsonplaceholder.typicode.com/users';
+  protected axios;
+
+  constructor() {
+    super();
+    this.axios = axios;
+  }
+
+private loadItems() {
+   if (!this.items.length) {
+     this.axios.get(this.url).then((response: AxiosResponse) => {
+       this.items = response.data;
+     }, (error) => {
+       console.error(error);
+     });
+   }
+}
 const _listItems = [
 ]
 
