07181c52e5ffd12e4d56057aac5fda4e58aece17
komei otake <komei1220@gmail.com>
2017-11-03 13:24:19 +0900

    add api mock data

--- template/src/api/listItems.ts
+++ template/src/api/listItems.ts
@@ -1,33 +1,5 @@
-import axios, {AxiosResponse} from 'axios';
+const data = require('./mock-data-list')
 
-interface UserResponse {
-  id: string;
-  name: string;
-}
-
-items: UserResponse[] = [];
-  private url = 'https://jsonplaceholder.typicode.com/users';
-  protected axios;
-
-  constructor() {
-    super();
-    this.axios = axios;
-  }
-
-private loadItems() {
-   if (!this.items.length) {
-     this.axios.get(this.url).then((response: AxiosResponse) => {
-       this.items = response.data;
-     }, (error) => {
-       console.error(error);
-     });
-   }
-}
-const _listItems = [
-]
-
-export default {
-  getListItems(cb) {
-    () => cb(_listItems);
-  },
+export function getAllList (cb) {
+  cb(data)
 }
---
+++ template/src/api/mock-data-list.ts
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: '0',
+    name: 'Bill',
+  },
+  {
+    id: '1',
+    name: 'Bill',
+  },
+  {
+    id: '2',
+    name: 'Bill',
+  },
+  {
+    id: '3',
+    name: 'Bill',
+  },
+  {
+    id: '4',
+    name: 'Bill',
+  },
+]
